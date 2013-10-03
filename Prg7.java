91d7c559c553b9c3b1ea07f53833dc5148e96767
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-10-03 12:13:45 +0100

    finished work on 7

--- Prg7.java
+++ Prg7.java
@@ -1,3 +1,6 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Prg7 {
 
 	public static void main ( String args[])
@@ -18,7 +21,7 @@ public class Prg7 {
 		int[] weight , value ;
 
 		x = scanner.nextLine ( ) ;
-		n = Integer.parseInt ( x ) ;
+		int n = Integer.parseInt ( x ) ;
 
 		weight = new int[n] ;
 		value = new int[n] ;
@@ -30,19 +33,58 @@ public class Prg7 {
 			value[i] = scanner.nextInt ( ) ;
 		}
 
-		piggy_bank = scanner.nextInt ( ) ;
+		int piggy_bank = scanner.nextInt ( ) ;
 
-		int[] x = new int[piggy_bank] ;
+		int[] v = new int[piggy_bank+5] ;
 
 		boolean updated = true ;
+		int coin_counter ;
+
+		//piggy_bank ;
+
+		for ( i = 0 ; i < piggy_bank ; ++ i )
+			v[i] = 0 ;
 
 		while ( updated )
 		{
 
 			updated = false ;
 
+			for ( coin_counter = 0 ; coin_counter < n ; ++ coin_counter )
+			{
+
+				int current_weight = weight [ coin_counter ] ;
+				int current_value = value [ coin_counter ] ;
+				for ( i = piggy_bank - current_weight ; i >= 0 ; -- i )
+					if ( v[i] > 0 )
+					{
+						int tmp = i + current_weight ; 
+						if ( v [ tmp ] > v[i] + current_value || v[ i + current_weight ] == 0)
+						{
+							v [ tmp ] = v[i] + current_value ;
+							updated = true ;
+						}
+					}	
+
+				if ( v[current_weight] > current_value || v[current_weight] == 0 )
+				{
+					v[current_weight] = current_value ;
+					updated = true ;
+				}
+			}
+			for ( i = 1 ; i <= piggy_bank ; ++ i )
+				System.out.print ( i + " " ) ;	
+			System.out.println ( ) ;			
+			for ( i = 1 ; i <= piggy_bank ; ++ i )
+				System.out.print ( v[i] + " " ) ;	
+
+			System.out.println ( ) ;
+			System.out.println ( ) ;
+		
 		}
 
+		for ( i = 0 ; i < piggy_bank ; ++ i )
+			System.out.println ( v[i] ) ;	
 		
 
 	}
